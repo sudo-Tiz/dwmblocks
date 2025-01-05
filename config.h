18994698dbffa88effccc8cafbf221af506fc874
sudo-Tiz <mathis.worksome@simplelogin.fr>
2025-01-05 21:19:09 +0100

    rm arch-audit

--- config.h
+++ config.h
@@ -2,9 +2,11 @@
 // recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-    {"", "sb-record", 10, 9},    /* {"", "cat /tmp/recordingicon 2>/dev/null", 0, 9}, */
+    {"", "sb-record", 10,9},
+    /* {"", "cat /tmp/recordingicon 2>/dev/null", 0, 9}, */
     {"", "sb-tasks", 30, 26},
     {"", "sb-music", 0, 11},
+    /* {"", "sb-audit", 0, 19}, */
     {"", "sb-pacpackages", 0, 8},
     {"", "sb-mailbox", 180, 12},
     {"", "sb-news", 0, 6},
@@ -21,23 +23,29 @@ static const Block blocks[] = {
     {"", "sb-volume", 0, 10},
     {"", "sb-battery", 30, 3},
     {"", "sb-internet", 5, 4},
-    /* {"", "sb-nextcloud", 0, 23}, */
+    {"", "sb-nextcloud", 0, 23},
     /* {"",	"sb-moonphase",	18000,	17}, */
     /*{"",	"sb-doppler",	0,	13},*/
     /* {"",	"sb-kbselect",		0,	30}, */
     /*{"",	"sb-help-icon",	0,	15},*/
-    /* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000, 22},*/
+    /* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,
+       22},*/
     /* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
-    /* {"",	"sb-price link \"Chainlink\" 🔗",			300, 25}, */
+    /* {"",	"sb-price link \"Chainlink\" 🔗",			300, 25},
+     */
     /* {"",	"sb-price xmr \"Monero\" 🔒",			9000,	24}, */
     /* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
-    /* {"",	"sb-price btc Bitcoin 💰",				9000, 21},*/
+    /* {"",	"sb-price btc Bitcoin 💰",				9000,
+       21},*/
 };
 
-// Sets delimiter between status commands. NULL character ('\0') means no delimiter.
+// Sets delimiter between status commands. NULL character ('\0') means no
+// delimiter.
 static char *delim = " ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
 
-// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
+// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd
+// ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid
+// dwmblocks & }
